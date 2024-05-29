53e30f3eb0e60120e4d9ada344d2a15e4c9b81a7
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-05-29 14:20:13 +0300

    Update header.blade.php

--- resources/views/livewire/elements/header.blade.php
+++ resources/views/livewire/elements/header.blade.php
@@ -50,7 +50,8 @@
                         <div class="collapse navbar-collapse pageMainNavCollapse mt-2 mt-md-0" id="pageMainNavCollapse">
                             <ul class="navbar-nav mainNavigation">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="/" onclick="event.preventDefault(); window.location.href = this.getAttribute('href');">Home</a>
+                                    <a class="nav-link" href="/"
+                                        onclick="event.preventDefault(); window.location.href = this.getAttribute('href');">Home</a>
                                 </li>
                                 <li class="nav-item dropdown ddohOpener">
                                     <a class="nav-link dropdown-toggle dropIcn" href="javascript:void(0);" role="button"
@@ -131,10 +132,12 @@
                                     </div>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{route('eservices')}}" onclick="event.preventDefault(); window.location.href = this.getAttribute('href');">E-Services</a>
+                                    <a class="nav-link" href="{{route('eservices')}}"
+                                        onclick="event.preventDefault(); window.location.href = this.getAttribute('href');">E-Services</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{route('projects')}}" onclick="event.preventDefault(); window.location.href = this.getAttribute('href');">Projects</a>
+                                    <a class="nav-link" href="{{route('projects')}}"
+                                        onclick="event.preventDefault(); window.location.href = this.getAttribute('href');">Projects</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{route('contact')}}"
@@ -145,7 +148,7 @@
                         <form action="#" class="hdSearchForm ml-3 ml-xl-6">
                             <a class="hdSearchOpener" data-toggle="collapse" href="#hdSearchCollapse" role="button"
                                 aria-expanded="false" aria-controls="hdSearchCollapse">
-                                <i class="icomoon-search"><span class="sr-only">search</span></i>
+                                <i class="fa fa-search">search</span></i>
                             </a>
                             <div class="collapse hdSearchCollapse d-block position-fixed" id="hdSearchCollapse">
                                 <div class="d-flex w-100 h-100 align-items-center jusity-content-center">
