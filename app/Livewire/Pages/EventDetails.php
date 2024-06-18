e9edee0a32b5748e970db5fd6dca4cb3ce9fa910
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-18 13:00:45 +0300

    Update event details

--- app/Livewire/Pages/EventDetails.php
+++ app/Livewire/Pages/EventDetails.php
@@ -2,14 +2,17 @@
 
 namespace App\Livewire\Pages;
 
+use App\Models\Event;
 use Livewire\Component;
 
 class EventDetails extends Component
 {
     public $slug;
     public $reference;
+
     public function render()
     {
-        return view('livewire.pages.event-details')->layout('layouts.base');
+        $event = Event::where('reference', $this->reference)->first();
+        return view('livewire.pages.event-details', ['event' => $event])->layout('layouts.base');
     }
 }
--- resources/views/livewire/pages/event-details.blade.php
+++ resources/views/livewire/pages/event-details.blade.php
@@ -1,6 +1,6 @@
 <main>
     @section('title')
-    Event Details
+    {{$event->title}}
     @endsection
     <header class="text-white pageMainHead d-flex position-relative bgCover w-100"
         style="background-image: url(https://placehold.co/1920x300);">
@@ -25,47 +25,41 @@
                 <div class="mb-6 col-12 col-lg-9">
                     <div class="pr-lg-5 pr-xl-15 bodyFontAlter">
                         <header class="mb-10 dtlItemHeader mt-n1">
-                            <strong class="dtlihTitle d-block">In <a href="javascript:void(0);">Category</a></strong>
-                            <h2 class="mb-9">Event Name</h2>
+                            <strong class="dtlihTitle d-block">In <a
+                                    href="javascript:void(0);">{{$event->category->name}}</a></strong>
+                            <h2 class="mb-9">{{$event->title}}</h2>
                             <div class="imgHolder">
-                                <img src="https://placehold.co/873x648" class="img-fluid w-100"
-                                    alt="image description">
+                                <img src="https://placehold.co/873x648" class="img-fluid w-100" alt="image description">
                             </div>
                             <address class="mb-0 dtlhDress fontAlter">
                                 <div class="row no-gutters">
                                     <div class="px-4 pt-6 pb-12 col-12 col-sm-4 px-md-7">
                                         <strong class="mb-2 d-block title fwSemiBold">Date</strong>
                                         <span class="d-block">
-                                            <time datetime="2011-01-12">January 14, 2020</time>
+                                            <time datetime="2011-01-12">{{date('M d,
+                                                Y',strtotime($event->start_date_and_time))}}</time>
                                         </span>
                                         <i class="fa fa-calendar icnWatermark"><span class="sr-only">icon</span></i>
                                     </div>
                                     <div class="px-4 pt-6 pb-12 col-12 col-sm-4 px-md-7">
                                         <strong class="mb-2 d-block title fwSemiBold">Time</strong>
                                         <span class="d-block">
-                                            <time datetime="2011-01-12">7:00 am - 8:45 am</time>
+                                            <time
+                                                datetime="2011-01-12">{{date('h:iA',strtotime($event->start_date_and_time))}}
+                                                - {{date('h:iA',strtotime($event->end_date_and_time))}}</time>
                                         </span>
                                         <i class="fa fa-clock icnWatermark"><span class="sr-only">icon</span></i>
                                     </div>
                                     <div class="px-4 pt-6 pb-12 col-12 col-sm-4 px-md-7">
                                         <strong class="mb-2 d-block title fwSemiBold">Location</strong>
-                                        <span class="d-block">Mayor Office, Norway County</span>
-                                        <i class="fa fa-map-marker icnWatermark"><span
-                                                class="sr-only">icon</span></i>
+                                        <span class="d-block"
+                                            style="text-transform: capitalize">{{$event->location}}</span>
+                                        <i class="fa fa-map-marker icnWatermark"><span class="sr-only">icon</span></i>
                                     </div>
                                 </div>
                             </address>
                         </header>
-                        <p class="mb-6">Numbers say it all. Globally, progress in the wind sector continues to be strong
-                            with increasing annual installed capaCounty and growing investment in the sector. In 2015
-                            alone, 63,013 megawatts of wind power capaCounty was installed globally an annual market
-                            growth of 22 percent. It is continuing its progress towards becoming a mainstream,
-                            competitive and reliable power source in both developing and mature markets. In fact, wind
-                            is becoming cheap enough in many places in the U.S. </p>
-                        <p class="mb-6">Yoga is a group of physical, mental, and spiritual practices or disciplines
-                            which originated in ancient India. There is a broad variety of Yoga schools, practices, and
-                            goals in Hinduism, Buddhism, and Jainism. Among the most well-known types of yoga are Hatha
-                            yoga and Raja yoga.</p>
+                        {!! $event->description !!}
                         <aside class="px-4 pt-5 pb-6 mb-8 countdownAside d-md-flex align-items-md-center mt-9">
                             <ul
                                 class="flex-wrap mb-4 list-unstyled cdScheduleList fontAlter mb-md-0 flex-grow-1 d-flex">
@@ -92,107 +86,19 @@
                                 <span class="d-block btnText">Participate Now</span>
                             </a>
                         </aside>
-                        <h3 class="mb-4 h3vvii fwSemiBold">Makeup Change the World</h3>
-                        <p class="mb-7">Numbers say it all. Globally, progress in the wind sector continues to be strong
-                            with increasing annual installed capaCounty and growing investment in the sector. In 2015
-                            alone, 63,013 megawatts of wind power capaCounty was installed globally an annual market
-                            growth of 22 percent. It is continuing its progress towards becoming a mainstream,
-                            competitive and reliable power source in both developing and mature markets. In fact, wind
-                            is becoming cheap enough in many places in the U.S. </p>
-                        <ol class="list-unstyled listCountDefault mb-9">
-                            <li>Thomas Edison may have been behind the invention.</li>
-                            <li>Edison worked alongside partners, both financial and commercial, to get his best
-                                inventions off the ground,</li>
-                            <li>Battling challenging cost targets and the need to build.</li>
-                            <li>Partnership with a supplier or original equipment manufacturer.</li>
-                        </ol>
-                        <h2 class="mb-5 h2vii fwSemiBold">Speakers &amp; Chief Guests</h2>
-                        <div class="row d-block mb-7 mb-lg-9 mb-xl-13">
-                            <div class="scgSpeakersSlider">
-                                <div class="col-12">
-                                    <article class="px-4 pt-6 mb-6 text-center bg-white shadow speakerColumn pb-7">
-                                        <div class="mb-3 imgHolder rounded-circle">
-                                            <img src="https://placehold.co/150x150"
-                                                class="img-fluid rounded-circle" alt="image description">
-                                        </div>
-                                        <h3 class="mb-1 fwMedium">Name Here</h3>
-                                        <h4 class="fontBase font-weight-normal">Destination</h4>
-                                    </article>
-                                </div>
-                                <div class="col-12">
-                                    <article class="px-4 pt-6 mb-6 text-center bg-white shadow speakerColumn pb-7">
-                                        <div class="mb-3 imgHolder rounded-circle">
-                                            <img src="https://placehold.co/150x150"
-                                                class="img-fluid rounded-circle" alt="image description">
-                                        </div>
-                                        <h3 class="mb-1 fwMedium">Name Here</h3>
-                                        <h4 class="fontBase font-weight-normal">Destination</h4>
-                                    </article>
-                                </div>
-                                <div class="col-12">
-                                    <article class="px-4 pt-6 mb-6 text-center bg-white shadow speakerColumn pb-7">
-                                        <div class="mb-3 imgHolder rounded-circle">
-                                            <img src="https://placehold.co/150x150"
-                                                class="img-fluid rounded-circle" alt="image description">
-                                        </div>
-                                        <h3 class="mb-1 fwMedium">Name Here</h3>
-                                        <h4 class="fontBase font-weight-normal">Destination</h4>
-                                    </article>
-                                </div>
-                                <div class="col-12">
-                                    <article class="px-4 pt-6 mb-6 text-center bg-white shadow speakerColumn pb-7">
-                                        <div class="mb-3 imgHolder rounded-circle">
-                                            <img src="https://placehold.co/150x150"
-                                                class="img-fluid rounded-circle" alt="image description">
-                                        </div>
-                                        <h3 class="mb-1 fwMedium">Name Here</h3>
-                                        <h4 class="fontBase font-weight-normal">Destination</h4>
-                                    </article>
-                                </div>
-                                <div class="col-12">
-                                    <article class="px-4 pt-6 mb-6 text-center bg-white shadow speakerColumn pb-7">
-                                        <div class="mb-3 imgHolder rounded-circle">
-                                            <img src="https://placehold.co/150x150"
-                                                class="img-fluid rounded-circle" alt="image description">
-                                        </div>
-                                        <h3 class="mb-1 fwMedium">Name Here</h3>
-                                        <h4 class="fontBase font-weight-normal">Destination</h4>
-                                    </article>
-                                </div>
-                                <div class="col-12">
-                                    <article class="px-4 pt-6 mb-6 text-center bg-white shadow speakerColumn pb-7">
-                                        <div class="mb-3 imgHolder rounded-circle">
-                                            <img src="https://placehold.co/150x150"
-                                                class="img-fluid rounded-circle" alt="image description">
-                                        </div>
-                                        <h3 class="mb-1 fwMedium">Name Here</h3>
-                                        <h4 class="fontBase font-weight-normal">Destination</h4>
-                                    </article>
-                                </div>
-                            </div>
-                        </div>
                         <ul class="mb-8 nav nav-tabs scgTabList fontAlter" id="scgTabs" role="tablist">
                             <li class="nav-item">
                                 <a class="nav-link active" id="location-tab" data-toggle="tab" href="#location"
                                     role="tab" aria-controls="location" aria-selected="true">Location</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" id="gallery-tab" data-toggle="tab" href="#gallery" role="tab"
+                                <a class="nav-link active" id="gallery-tab" data-toggle="tab" href="#gallery" role="tab"
                                     aria-controls="gallery" aria-selected="false">Gallery</a>
                             </li>
                         </ul>
                         <div class="mb-10 tab-content scgTabContent" id="scgTabsContent">
-                            <div class="tab-pane fade show active" id="location" role="tabpanel"
-                                aria-labelledby="location-tab">
-                                <div class="overflow-hidden">
-                                    <div class="ctMapWrap position-relative w-100">
-                                        <iframe
-                                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3117.246590958436!2d-90.29455418438972!3d38.62020787083965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87d8b57ada367f63%3A0xe3ea4c84d3e6b420!2sC%20%26%20B%20Lift%20Truck%20Service%2C%20Inc.!5e0!3m2!1sen!2s!4v1630908088272!5m2!1sen!2s"
-                                            width="100%" height="498" allowfullscreen="" loading="lazy"></iframe>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="tab-pane fade" id="gallery" role="tabpanel" aria-labelledby="gallery-tab">
+                            <div class="tab-pane fade show active" id="gallery" role="tabpanel"
+                                aria-labelledby="gallery-tab">
                                 <div class="overflow-hidden">
                                     <ul class="flex-wrap list-unstyled d-flex scgGalleryList">
                                         <li>
@@ -289,8 +195,8 @@
                                 <article class="bg-white shadow ueEveColumn position-relative w-100">
                                     <div class="imgHolder position-relative">
                                         <a href="{{route('event.details')}}">
-                                            <img src="https://placehold.co/383x285"
-                                                class="img-fluid d-block w-100" alt="image description">
+                                            <img src="https://placehold.co/383x285" class="img-fluid d-block w-100"
+                                                alt="image description">
                                         </a>
                                         <time
                                             class="text-center bg-white ueTimeTag position-absolute d-flex fontAlter fwSemiBold text-lDark text-uppercase"
@@ -305,7 +211,7 @@
                                             class="mb-2 d-block ueCatTitle fwSemiBold text-secondary">Category</strong>
                                         <h3 class="mb-3 h3Small fwMedium">
                                             <a href="{{route('event.details')}}">Event Name
-                                                </a>
+                                            </a>
                                         </h3>
                                         <address>
                                             <ul class="pl-0 list-unstyled ueScheduleList">
