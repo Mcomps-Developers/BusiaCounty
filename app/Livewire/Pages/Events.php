ac351aa7f74a1fde4f5f7d3447e9c283b1fb5c56
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-18 12:35:54 +0300

    Update Events.php
    Display events

--- app/Livewire/Pages/Events.php
+++ app/Livewire/Pages/Events.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire\Pages;
 
+use App\Models\Event;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Events extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.pages.events')->layout('layouts.base');
+        $events = Event::orderByDesc('created_at')->paginate(12);
+        return view('livewire.pages.events', ['events' => $events])->layout('layouts.base');
     }
 }
