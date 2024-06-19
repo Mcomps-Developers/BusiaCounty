a85b976f03a8d01f940b65a2b7dbfcdb3e2e31ea
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-19 11:41:30 +0300

    Update home-component.blade.php

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -239,7 +239,7 @@
                         <span class="img img1 position-relative d-block">
                             @if ($note->photo)
                             <img src="https://busia.mcomps.africa/assets/img/governors/{{$note->photo}}"
-                                class="img-fluid" height="650" width="600">
+                                class="img-fluid" height="500" width="400">
                             @else
                             <img src="https://placehold.co/600x650" class="img-fluid">
                             @endif
