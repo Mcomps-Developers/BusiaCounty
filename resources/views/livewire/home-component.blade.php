e62af3e05bb9a84cc09d65c57c590fb46b6ef368
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-19 16:08:38 +0300

    Update home-component.blade.php

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -65,7 +65,7 @@
                     <div class="pt-20 pb-20 align w-100 pt-md-40 pb-md-30 px-md-17">
                         <div class="container position-relative">
                             <div class="row">
-                                <div class="col-12 col-md-9 col-xl-7 fzMedium" style="height: 200px;">
+                                <div class="col-12 col-md-9 col-xl-7 fzMedium" style="height: relative; width:100%;">
                                     {{-- <h1 class="mb-4 text-white h1Large">Governor's Inspection at Busia County
                                         Referral
                                         Hospital</h1> --}}
