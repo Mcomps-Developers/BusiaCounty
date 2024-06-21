bcf1aaab80d473b0d3a047946ceee5622e35cbb8
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-21 15:29:29 +0300

    Update deputy-governor.blade.php

--- resources/views/livewire/pages/deputy-governor.blade.php
+++ resources/views/livewire/pages/deputy-governor.blade.php
@@ -24,7 +24,8 @@
             <header
                 class="pt-4 overflow-hidden bg-white shadow tmcbDescriptionBox d-lg-flex pt-lg-0 position-relative mb-7 mb-md-10 mb-lg-15">
                 <img class="flex-shrink-0 mx-auto imgHolder bgCover mx-lg-0 d-block"
-                    src="https://busia.mcomps.africa/assets/img/about/deputy-governor/{{$deputyGovernor->photo}}" style="height: relative"></img>
+                    src="https://busia.mcomps.africa/assets/img/about/deputy-governor/{{$deputyGovernor->photo}}"
+                    style="background-position: 50% relative;"></img>
                 <div class="px-5 pt-6 pb-6 descrWrap flex-grow-1 px-xl-12 pt-xl-9 pb-xl-12">
                     <div class="row">
                         <div class="col-12 col-md-5">
@@ -77,7 +78,10 @@
                     </div>
                 </div>
             </header>
-            {!! $deputyGovernor->about !!}
+            <header
+                class="pt-4 overflow-hidden bg-white shadow tmcbDescriptionBox d-lg-flex pt-lg-0 position-relative mb-7 mb-md-10 mb-lg-15">
+                {!! $deputyGovernor->about !!}
+            </header>
         </div>
     </article>
 </main>
