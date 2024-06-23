7af23d773189cca9e35ee7852e697261e6975703
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-23 14:42:34 +0300

    Update governor-note.blade.php

--- resources/views/livewire/pages/governor-note.blade.php
+++ resources/views/livewire/pages/governor-note.blade.php
@@ -23,9 +23,8 @@
         <div class="container">
             <header
                 class="pt-4 overflow-hidden bg-white shadow tmcbDescriptionBox d-lg-flex pt-lg-0 position-relative mb-7 mb-md-10 mb-lg-15">
-                <img class="flex-shrink-0 mx-auto imgHolder bgCover mx-lg-0 d-block"
-                    src="https://busia.mcomps.africa/assets/img/about/governor/{{$Governor->photo}}" height="relative"
-                    width="relative"></img>
+                <img class="img-fluid d-block w-100"
+                    src="https://busia.mcomps.africa/assets/img/about/governor/{{$Governor->photo}}"></img>
                 <div class="px-5 pt-6 pb-6 descrWrap flex-grow-1 px-xl-12 pt-xl-9 pb-xl-12">
                     <div class="row">
                         <div class="col-12 col-md-5">
