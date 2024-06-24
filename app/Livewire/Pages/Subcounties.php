7269305f16e94d6e0a73fac23aca1db18d3bad43
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-24 11:15:38 +0300

    Update Subcounties.php

--- app/Livewire/Pages/Subcounties.php
+++ app/Livewire/Pages/Subcounties.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire\Pages;
 
+use App\Models\Subcounty;
 use Livewire\Component;
 
 class Subcounties extends Component
 {
     public function render()
     {
-        return view('livewire.pages.subcounties')->layout('layouts.base');
+        $subcounties = Subcounty::orderBy('name')->get();
+        return view('livewire.pages.subcounties', ['subcounties' => $subcounties])->layout('layouts.base');
     }
 }
