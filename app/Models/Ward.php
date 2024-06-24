39c7c34a9ef3bcbbfd92eae69bb5b0641662680b
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-06-24 17:29:39 +0300

    Update Ward.php

--- app/Models/Ward.php
+++ app/Models/Ward.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Ward extends Model
 {
     use HasFactory;
+    public function subCounty(){
+        return $this->belongsTo(Subcounty::class,'subcounty_id');
+    }
 }
