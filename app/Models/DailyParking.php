6a177b36559afd1437d54d00f1320edc79738aaa
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-28 14:43:47 +0300

    BugFix

--- app/Models/DailyParking.php
+++ app/Models/DailyParking.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DailyParking extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function vehicleType(){
         return $this->belongsTo(Vehicle::class,'vehicle_id');
