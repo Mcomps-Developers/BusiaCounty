eb10082ccb7783de67d5781acb3b9e4702da6615
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-30 08:52:35 +0300

    Update home-component.blade.php

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -113,7 +113,7 @@
     </div>
     <aside class="text-white featuresAsideBlock position-relative">
         <div class="container">
-            <div class="flatpWrap position-relative mt-n8 mt-md-n18" style="transform: scale(0.75)">
+            <div class="flatpWrap position-relative mt-n8 mt-md-n18" style="transform: scale(0.6)">
                 <ul class="flex-wrap mb-0 overflow-hidden list-unstyled fabFeaturesList d-flex">
                     <li>
                         <a href="{{ route('executive') }}" class="px-2 pt-4 pb-10 text-center fflColumn d-block w-100">
