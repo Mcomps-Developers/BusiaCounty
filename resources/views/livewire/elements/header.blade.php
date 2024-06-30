e84bfbda58f10df262c2c434996a8dbfe49645a1
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-30 08:05:54 +0300

    Update header.blade.php

--- resources/views/livewire/elements/header.blade.php
+++ resources/views/livewire/elements/header.blade.php
@@ -48,7 +48,7 @@
                     <div
                         class="hdNavWrap flex-grow-1 d-flex align-items-center justify-content-end justify-content-lg-start">
                         <div class="mt-2 collapse navbar-collapse pageMainNavCollapse mt-md-0" id="pageMainNavCollapse">
-                            <ul class="navbar-nav mainNavigation">
+                            <ul class="navbar-nav mainNavigation" style="color:white">
                                 <li class="nav-item">
                                     <a class="nav-link" href="/"
                                         onclick="event.preventDefault(); window.location.href = this.getAttribute('href');">Home</a>
