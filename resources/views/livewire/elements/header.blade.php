fe8ae1275158ee01f6296e56f4224272b8536971
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-30 08:14:15 +0300

    Update header.blade.php

--- resources/views/livewire/elements/header.blade.php
+++ resources/views/livewire/elements/header.blade.php
@@ -37,7 +37,7 @@
                 </div>
             </div>
         </div>
-        <div class="py-2 bg-dark hdFixerWrap py-md-3 py-xl-5 sSticky">
+        <div class="py-2 hdFixerWrap py-md-3 py-xl-5 sSticky" style="background-color: #9ECA3E">
             <div class="container">
                 <nav class="p-0 navbar navbar-expand-md navbar-dark">
                     <div class="flex-shrink-0 mr-3 logo mr-xl-8 mr-xlwd-16">
