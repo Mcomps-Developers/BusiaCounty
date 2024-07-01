37c5e4619fdf54cd62311b694721df6b7ecfb89f
petermuremi351 <mcompsnetwork@gmail.com>
2024-07-01 11:34:20 +0300

    Update governor-note.blade.php

--- resources/views/livewire/pages/governor-note.blade.php
+++ resources/views/livewire/pages/governor-note.blade.php
@@ -139,6 +139,58 @@
                         <p>Meet the department leadership team.</p>
                     </header>
                     <div class="row justify-content-center">
+                        <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+                            <article class="mx-auto mb-6 bg-white shadow mccColumn mx-sm-0">
+                                <div class="imgHolder position-relative">
+                                    <img src="https://busia.mcomps.africa/assets/img/about/deputy-governor/{{$deputyGovernor->photo}}"
+                                        class="img-fluid d-block w-100" height="255" width="255">
+                                    <div class="mcssHolder">
+                                        <ul class="p-0 m-0 overflow-hidden bg-white mcssList list-unstyled rounded-pill d-flex">
+                                            <li>
+                                                <a href="{{$deputyGovernor->twitter}}" class="mcssLink" title="Twitter"><i
+                                                        class="vssIcn fab fa-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="{{$deputyGovernor->facebook}}" class="mcssLink" title="Facebook"><i
+                                                        class="vssIcn fab fa-facebook-square"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="{{$deputyGovernor->linkedin}}" class="mcssLink" title="Linkedin"><i
+                                                        class="vssIcn fab fa-linkedin-in"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="{{$deputyGovernor->instagram}}" class="mcssLink" title="Instagram"><i
+                                                        class="vssIcn fab fa-instagram"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="px-5 pt-5 pb-4 mcCaptionWrap position-relative">
+                                    <h3 class="mb-1 fwMedium h3Small">{{$deputyGovernor->name}}</h3>
+                                    <h4 class="fwSemiBold fontBase text-secondary"><a href="{{route('deputy.governor')}}">Deputy
+                                            Governor</a></h4>
+                                    <hr class="mx-0 mt-4 mb-3 mccSeprator">
+                                    <ul class="list-unstyled mccInfoList">
+                                        @if ($deputyGovernor->email)
+                                        <li>
+                                            <a href="mailto:{{$deputyGovernor->email}}">
+                                                <i class="mr-1 fas fa-envelope icn"><span class="sr-only">icon</span></i>
+                                                {{$deputyGovernor->office_email}}
+                                            </a>
+                                        </li>
+                                        @endif
+                                        @if ($deputyGovernor->office_phone)
+                                        <li>
+                                            <a href="tel:+{{$deputyGovernor->office_phone}}">
+                                                <i class="mr-1 fas fa-phone-alt icn"><span class="sr-only">icon</span></i>
+                                                +{{$deputyGovernor->office_phone}}
+                                            </a>
+                                        </li>
+                                        @endif
+                                    </ul>
+                                </div>
+                            </article>
+                        </div>
                         @foreach ($officers as $item)
                             <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
                                 <article class="mx-auto mb-6 bg-white shadow mccColumn mx-sm-0">
