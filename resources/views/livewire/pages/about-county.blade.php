c9dff99b63ed6681d5bc8c421f6345bdabeea95c
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-08-03 18:01:11 +0300

    Update about-county.blade.php

--- resources/views/livewire/pages/about-county.blade.php
+++ resources/views/livewire/pages/about-county.blade.php
@@ -24,27 +24,10 @@
                 <div class="mb-6 col-12 col-lg-8 col-xl-9 order-lg-2">
                     <div class="pl-xl-14">
                         <header class="fzMedium mb-9">
-                            <h2 class="fwSemiBold h2vii">About {{ config('app.name') }}</h2>
+                            <h2 class="fwSemiBold h2vii">The History of Busia County</h2>
                         </header>
                         {!! $content->content !!}
                     </div>
-                </div>
-                <div class="mb-6 col-12 col-lg-4 col-xl-3 order-lg-1">
-                    <aside class="pt-1 dscSidebar mr-xl-n5">
-                        <div class="px-6 pt-5 pb-8 mb-6 widget mb-lg-10 widgetHelp bg-lDark position-relative">
-                            <i class="mb-3 text-white icnWrap icomoon-chatq d-block"><span
-                                    class="sr-only">icon</span></i>
-                            <h3 class="mb-2 text-white h3Medium">Need any help?</h3>
-                            <p>Here you can get your perfect answer for your problem.</p>
-                            <a href="{{ route('contact') }}"
-                                class="p-0 border-0 btn btnTheme btn-sm font-weight-bold text-capitalize position-relative"
-                                data-hover="Contact now">
-                                <span class="d-block btnText">Contact now</span>
-                            </a>
-                            <i class="whWatermarkIcn icomoon-helpc position-absolute"><span
-                                    class="sr-only">icon</span></i>
-                        </div>
-                    </aside>
                 </div>
             </div>
         </div>
