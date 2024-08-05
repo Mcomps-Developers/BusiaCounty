10eda132322acebcf4155465b32fc107e5ad291f
Manzolo <83277755+AlmeidaManzolo@users.noreply.github.com>
2024-08-05 09:43:47 -0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'The County Government of Busia'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://busiacounty.go.ke'),
 
     /*
     |--------------------------------------------------------------------------
